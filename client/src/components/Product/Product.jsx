46fbe0106192b55eeccf16f648934c1f616be3df
yavordobrev24 <yavordobrev24@gmail.com>
2023-11-05 22:24:49 +0200

    SearchBar and Product changes
    Added additional styling to the min and max filters in the SearchBar component. Changed the cards styling of the Product component.

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -3,15 +3,23 @@ export default function Product(props) {
   const addToCart = (e) => {
     console.log(props._id);
   };
+  const showDetails = (e) => {
+    console.log("works");
+  };
   return (
     <div className="product">
       <img src={props.url} alt={props.name} />
       <h3>{props.name}</h3>
       <p className="description">{props.description}</p>
       <p className="price">${props.price}</p>
-      <button className="add-to-cart-button" onClick={addToCart}>
-        Add to Cart
-      </button>
+      <div className="product-buttons">
+        <button className="add-to-cart-button" onClick={addToCart}>
+          Add to Cart
+        </button>
+        <button className="details-button" onClick={showDetails}>
+          Details
+        </button>
+      </div>
     </div>
   );
 }
