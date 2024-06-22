d8f5ecfe5f7b51af60dfefe689214b126d30df25
Yavor Dobrev <yavordobrev24@gmail.com>
2024-06-23 01:05:05 +0300

    create Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,6 @@
+import styles from "./Button.module.css";
+export default function Button({children,props}) {
+  return (
+    <button {...props} className={styles.button}>{children}</button>
+  )
+}
