5c63ceb46f4d2134bef9192eb80d52a61ccfccce
Yavor Dobrev <yavordobrev24@gmail.com>
2024-06-23 01:04:55 +0300

    update ShoppingCart component

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,42 +1,39 @@
 import { useContext, useEffect, useState } from "react";
 import styles from "./ShoppingCart.module.css";
 import AuthContext from "../../contexts/authContext";
-import { deleteCart, getCartItems } from "../../services/cartService";
+import Button from "../Button/Button";
+import Quantity from "../Quantity/Quantity";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function ShoppingCart() {
-  const { userId, onDeleteCart, onDeleteCartItem } = useContext(AuthContext);
+  const { emptyCart, isAuthenticated, removeFromCart, cart } =
+    useContext(AuthContext);
   const [showModal, setShowModal] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
-
-  useEffect(() => {
-    const fetchCartItems = async () => {
-      const data = await getCartItems(userId);
-      setCartItems(data);
-    };
-    fetchCartItems();
-  }, [onDeleteCartItem]);
+  const navigate = useNavigate();
   const calculateTotal = () => {
-    return cartItems?.reduce((total, ci) => {
-      return total + Number(ci.products.price);
+    return cart?.reduce((total, cartItem) => {
+      return total + Number(cartItem.price * cartItem.quantity);
     }, 0);
   };
 
   const closeModal = () => {
     setShowModal(false);
+    navigate("/");
   };
 
   return (
     <div className={styles.cart}>
       <h1>Your Shopping Cart</h1>
       <div className={styles["cart-items"]}>
-        {cartItems?.length > 0 ? (
-          cartItems.map((ci) => (
-            <div key={ci.id} className={styles["cart-item"]}>
-              <img src={ci.products.image_url} alt={ci.products.name} />
-              <h3 className={styles.name}>{ci.products.name}</h3>
-              <p className={styles.price}>${ci.products.price}</p>
+        {cart?.length > 0 ? (
+          cart.map((cartItem) => (
+            <div key={cartItem.id} className={styles["cart-item"]}>
+              <img src={cartItem.image_url} alt={cartItem.name} />
+              <h3 className={styles.name}>{cartItem.name}</h3>
+              <p className={styles.price}>${cartItem.price}</p>
+              <Quantity cartItem={cartItem} />
               <button
-                onClick={() => onDeleteCartItem(ci.id)}
+                onClick={() => removeFromCart(cartItem.id)}
                 className={styles["remove-btn"]}
               >
                 X
@@ -44,26 +41,31 @@ export default function ShoppingCart() {
             </div>
           ))
         ) : (
-          <p className={styles.no}>There are no any items in your cart.</p>
+          <p className={styles.no}>There are no items in your cart.</p>
         )}
       </div>
       <div className={styles.total}>
         <p>
           Total:{" "}
-          <span>${cartItems.length > 0 ? calculateTotal().toFixed(2) : 0}</span>
+          <span>${cart.length > 0 ? calculateTotal().toFixed(2) : 0}</span>
         </p>
       </div>
-      {cartItems.length > 0 ? (
-        <button
-          className={styles.checkout}
-          onClick={() => {
-            setShowModal(true);
-            onDeleteCart(userId);
-            setCartItems([]);
-          }}
-        >
-          Checkout
-        </button>
+      {cart.length > 0 ? (
+        isAuthenticated ? (
+          <button
+            className={styles.checkout}
+            onClick={() => {
+              setShowModal(true);
+              emptyCart();
+            }}
+          >
+            Checkout
+          </button>
+        ) : (
+          <Button>
+            <Link to="/login">Authenticate to checkout</Link>
+          </Button>
+        )
       ) : null}
 
       {showModal && (
